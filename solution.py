bb503244e9adbb15d8784c96156a2500860ede81
surafel <72102647+surafel58@users.noreply.github.com>
2023-07-27 11:28:42 +0300

    create alternate solution for the problem

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
-         
-        arr.sort()
         
-        if arr[0] != 1:
-            arr[0] = 1
+        arr.sort(reverse=True)
+        
+        if arr[-1] != 1:
+            arr[-1] = 1
 
-        for i in range(1, len(arr)):
-            if abs(arr[i] - arr[i - 1]) > 1:
-                arr[i] = arr[i - 1] + 1
+        for i in range(len(arr) - 2, 0, -1):
+            if abs(arr[i] - arr[i + 1]) > 1:
+                arr[i] = arr[i + 1] + 1
 
-        return arr[-1]
+        return arr[0]
