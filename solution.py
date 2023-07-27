f314c483e04328a067e4efa73d444770fb0c62ee
surafel <72102647+surafel58@users.noreply.github.com>
2023-07-27 11:31:19 +0300

    create alternate solution

--- solution.py
+++ solution.py
@@ -11,3 +11,16 @@ class Solution:
                 arr[i] = arr[i - 1] + 1
 
         return arr[-1]
+
+
+        #an alternative solution
+        arr.sort(reverse=True)
+        
+        if arr[-1] != 1:
+            arr[-1] = 1
+
+        for i in range(len(arr) - 2, 0, -1):
+            if abs(arr[i] - arr[i + 1]) > 1:
+                arr[i] = arr[i + 1] + 1
+
+        return arr[0]
